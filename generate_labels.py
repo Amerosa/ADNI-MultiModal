2059bae11d996c51e5447ce9f6837d91d8d2be39
amerosa <giuseppe.volpe.grossi@gmail.com>
2019-12-12 10:41:39 -0500

    Minor fix to label genration

--- generate_labels.py
+++ generate_labels.py
@@ -5,31 +5,25 @@ parser = argparse.ArgumentParser(description='Generate a clean tsv file with num
 parser.add_argument('tsv', metavar='FILE', help='Tsv file generated from clinica iotools merge pipeline')
 args = parser.parse_args()
 
-master = pd.read_csv(args.tsv, sep='\t', usecols=['participant_id','session_id','diagnosis_bl','diagnosis'])
-#new_master = pd.DataFrame(columns=['participant_id','session_id','diagnosis'])
-#print(new_master)
+master = pd.read_csv(args.tsv, sep='\t', usecols=['participant_id','session_id','diagnosis_bl','diagnosis', 'age'])
+master = master.sort_values(by=['participant_id','session_id'])
+master.reset_index(drop=True, inplace=True)
+
+
+threshold = 3.0 #36 months equals 3 years can adjust to something else
 for i, row in enumerate(master.itertuples()):
     
-    if pd.isna(row.diagnosis):
-        
-        if row.diagnosis_bl == 'LMCI':
-            temp = master.loc[master['participant_id'] == row.participant_id]
-        
-            if temp['diagnosis'].isin(['AD']).any():
-                master.at[i, 'diagnosis'] = 'cMCI'
-            else:
-                master.at[i, 'diagnosis'] = 'nMCI'
-        else:
-            master.at[i, 'diagnosis'] = row.diagnosis_bl
-    
- 
-    if row.diagnosis == 'MCI' or row.diagnosis == 'LMCI':
+    if row.diagnosis == 'MCI':
         temp = master.loc[master['participant_id'] == row.participant_id]
-        #print(i, temp)
+        #temp = temp.loc[temp['age'] <= row.age + 3]
+
+        print(i, temp)
         if temp['diagnosis'].isin(['AD']).any():
             master.at[i, 'diagnosis'] = 'cMCI'
         else:
             master.at[i, 'diagnosis'] = 'nMCI'
+
+#print(master.sort_values(by=['participant_id','session_id']))
 
 if master.to_csv('./labels.tsv', sep='\t', index=False):
     print("File saved successfully!")
