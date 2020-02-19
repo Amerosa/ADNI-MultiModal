0651d85dbd4f59b8266b7deebe74dbaef6bb4582
amerosa <giuseppe.volpe.grossi@gmail.com>
2020-02-19 15:17:44 -0500

    Finished the CLI for creating subsets of images

--- dataset_split.py
+++ dataset_split.py
@@ -4,78 +4,102 @@ import nibabel as nib
 from PIL import Image
 import numpy as np
 from pathlib import Path
-from skimage.measure import shannon_entropy 
+from skimage.measure import shannon_entropy
+import glob
+import argparse
 
+parser = argparse.ArgumentParser(description='Create a subset of data basing slices off entropy significance.')
+parser.add_argument('threshold', type=int, help='The amount of important slices you want from the volume')
+parser.add_argument('--random', default=False, action='store_true', help='Enable this to use for random slice indexing instead of entropy')
+args = parser.parse_args()
 
 dataset_root = "F:/caps/subjects/"
 
-subjects = os.listdir("F:/caps/subjects/")
-print(len(subjects))
+if args.random:
+    master_root = "F:/temp/" + "random_" + str(args.threshold)
+else:
+    master_root = "F:/temp/" + "entropy_" + str(args.threshold)
 
+print("Images will be store in", master_root)
 
-#For each subject we need to create a file with thier name and then create to child dirs mri and pet
-#Then we populate with the entropy thresholds and random
-#For one subject we need to grab the right image and process it with nibabel
-
-def make_paths(root_dir, pattern):
-    file_paths = []
-    for root, dirs, files in os.walk(root_dir, topdown=False):
-        for file in files:
-            if fnmatch.fnmatch(file, pattern):
-                file_paths.append( (os.path.join(root, file)) )
-    
-    return file_paths
-
-def get_subject_image(subject_path):
-    img = nib.load(subject_path) #nii file type medical image
+def get_image(path):
+    img = nib.load(path) #nii file type medical image
     return img.get_fdata() #Converted to a numpy array type
-    #Final conversion to PIL images type for easy saving later, also need to be in 0-255 to convert to PIL
-
-#TODO make this return an image array and then we can bundle in another functions the slices and what not    
-
-def save_slices(destination, image, threshold=30, rand=False):
     
-    if rand:
-        #Document says for randint value may be one plus the highest so I may be indexing from
-        indicies = list(np.random.randint(image.shape[1], size=threshold)) 
+def shannon(image):
+    entropies = [ (shannon_entropy(image[:,i,:]), i) for i in range(image.shape[1]) ]
+    entropies = sorted(entropies, key=lambda x: x[0], reverse=True)
+    return list(zip(*entropies[:args.threshold]))
+
+def random_entropy(shape):
+    return list(np.random.randint(shape, size=args.threshold)) 
+
+def save_images(mri_destination, pet_destination, mri_image_path, pet_image_path):
+   
+    mri_image = get_image(mri_image_path)
+    pet_image = get_image(pet_image_path)
+
+    if args.random:
+        indicies = random_entropy(mri_image.shape[1]) #Axial slice 
     else:
-        entropies = [ (shannon_entropy(image[:,i,:]), i) for i in range(image.shape[1]) ]
-        entropies = sorted(entropies, key=lambda x: x[0], reverse=True)
-        entropies = list(zip(*entropies[:threshold]))
-        indicies = list(entropies[1])
+        indicies = list(shannon(mri_image)[1])
 
+    for sli in indicies:
+        mri_temp = mri_image[:,sli,:]
+        pet_temp = pet_image[:,sli,:]
+
+        mri_temp = Image.fromarray( (mri_temp*255).astype(np.uint8) )
+        pet_temp = Image.fromarray( (pet_temp*255).astype(np.uint8) )
+
+        mri_temp.save(os.path.join(mri_destination, str(sli) + ".png"))
+        pet_temp.save(os.path.join(pet_destination, str(sli) + ".png"))
     
-    for index in indicies:
-        temp = image[:,index,:]
-        temp = Image.fromarray( (temp*255).astype(np.uint8) )
-        temp.save(os.path.join(destination, str(index) + ".png"))
-    
-    
-for experiment in ['entropy', 'random']:
-    
-    subject_list = os.listdir(dataset_root)
-    print(subject_list)
-    for subject in subject_list:
-        for sess in os.listdir(os.path.join(dataset_root, subject)):
-            for mode in ['mri', 'pet']:
+def join_subject_lists(mri, pet):
+    temp = []
+    for i, subject in enumerate(mri):
+        temp.append( (*subject, pet[i][2] ) ) #Indexing the same subject but getting the pet path
+    return temp
+        
+def get_paths(dataset_root):
+    mri_subjects = []
+    pet_subjects = []
+
+    for root, dirs, files in os.walk(dataset_root):
+        for file in files:
+
+            sub_id, session = file.split('_')[:2]
+        
+            if fnmatch.fnmatch(file, "*segm-graymatter*fwhm-8mm*"):
+                mri_path = os.path.join(root, file )
+                mri_subjects.append( (sub_id, session, mri_path) )
+
+            if fnmatch.fnmatch(file, "*pet*fwhm-8mm*"):
+                pet_path = os.path.join(root, file )
+                pet_subjects.append( (sub_id, session, pet_path) )
                 
-                new_path = os.path.join("F:/interm/",experiment, subject, sess, mode)
-                if not os.path.exists(new_path):
-                    os.makedirs(new_path)
-                
-                #TODO need to populate the folders I made properly 
-                for root, dirs, files in os.walk(os.path.join(dataset_root, subject, sess), topdown=False):
-                    for file in files:
-                        if fnmatch.fnmatch(file, pattern):
-                           image_path = os.path.join(root, file)
 
+    return join_subject_lists(mri_subjects, pet_subjects)
 
+def create_subject_dir(subject):
 
+    sub_id, session, _, _ = subject
+    new_mri_path = os.path.join(master_root, sub_id, session, 'mri')
+    new_pet_path = os.path.join(master_root, sub_id, session, 'pet')
 
-# for root, dirs, files in os.walk(dataset_root):
-#     for file in files:
-#         if fnmatch.fnmatch(file, "*_T1w_segm-graymatter_space-Ixi549Space_modulated-on_fwhm-8mm_probability.nii.gz"):
-#             print(os.path.join(root, file)) 
+    if not os.path.exists(new_mri_path):
+        os.makedirs(new_mri_path)
 
-# img = get_subject_image("F:\caps\subjects\sub-ADNI006S1130\ses-M48\\t1\spm\dartel\group-MultiMode\sub-ADNI006S1130_ses-M48_T1w_segm-graymatter_space-Ixi549Space_modulated-on_fwhm-8mm_probability.nii.gz")
-# save_slices("F:/temp", img, threshold=30, rand=True)
+    if not os.path.exists(new_pet_path):
+        os.makedirs(new_pet_path)
+
+    return new_mri_path, new_pet_path
+
+def main():
+    subjects = get_paths(dataset_root)
+    for subject in subjects:
+        mri_path, pet_path = create_subject_dir(subject)
+        save_images(mri_path, pet_path, subject[2], subject[3])
+
+main()
+print()
+print("Finished Creating Subset!")
